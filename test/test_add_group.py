c7e8f55aa2553ed4732a9c69d779f7d0d3d8082b
Vlad Moyseienko <vladbrutal@gmail.com>
2019-07-26 01:02:44 +0300

    1. Distinguish auxiliary class "Session helper" for group and for contact

---
+++ test/test_add_group.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.group import Group
+from fixture.application_group import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.create_group(Group(name="Home Work 1", header="HW Header 1", footer="HW Footer 1"))
+    app.session.logout()
+
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.session.logout()
