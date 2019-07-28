aad9542f0e274e0eb53c6aba3de79c8457b67a80
Vlad Moyseienko <vladbrutal@gmail.com>
2019-07-28 13:01:19 +0300

    uncomment test_edit_group methods

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,12 +7,13 @@ def test_edit_group_name(app):
     app.session.logout()
 
 
-# def test_edit_group_header(app):
-#     app.session.login(username="admin", password="secret")
-#     app.group.edit_first_group(Group(name="New Header"))
-#     app.session.logout()
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="New Header"))
+    app.session.logout()
 
-# def test_edit_group_footer(app):
-#     app.session.login(username="admin", password="secret")
-#     app.group.edit()
-#     app.session.logout()
+
+def test_edit_group_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="New Footer"))
+    app.session.logout()
