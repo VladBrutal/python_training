eab1e890b09bf523bc2b833029774dd07853ea75
Vlad Moyseienko <vladbrutal@gmail.com>
2019-07-28 12:16:43 +0300

    In class group we changed method fill_group_form by extracting method "change_field_value" from it and gave him additional parameter "field_name"

--- fixture/group.py
+++ fixture/group.py
@@ -18,20 +18,23 @@ class GroupHelper:
         self.return_to_groups_page()
 
     def fill_group_form(self, group):
-        wd = self.app.wd
-        if group.name is None:
-            pass
-        else:
-            wd.find_element_by_name("group_name").click()
-            wd.find_element_by_name("group_name").clear()
-            wd.find_element_by_name("group_name").send_keys(group.name)
-
+        # wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
         # wd.find_element_by_name("group_header").click()
         # wd.find_element_by_name("group_header").clear()
         # wd.find_element_by_name("group_header").send_keys(group.header)
         # wd.find_element_by_name("group_footer").click()
         # wd.find_element_by_name("group_footer").clear()
         # wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
         wd = self.app.wd
