24c0004356648c313b72683f5887ad1e38a32498
Vlad Moyseienko <vladbrutal@gmail.com>
2019-09-17 23:14:17 +0300

    HW 16   fixture->application:   1. Was realize options of command line, that allows to choice between browsers when running a test.   conftest.py   1. Was realized hook function, which allows to run a browser from command line and url

--- fixture/application.py
+++ fixture/application.py
@@ -6,13 +6,20 @@ from fixture.session import SessionHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-        # self.wd = webdriver.Chrome()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(4)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
