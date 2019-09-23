df6e0f03ed8c0ae37f9dff740f0cb4c620bc8dff
Vlad Moyseienko <vladbrutal@gmail.com>
2019-09-23 17:24:02 +0300

    AttributeError: 'WebDriver' object has no attribute 'get_logged_user'

--- fixture/application.py
+++ fixture/application.py
@@ -15,7 +15,7 @@ class Application:
             self.wd = webdriver.Safari()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
-        self.wd.implicitly_wait(4)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,12 +5,6 @@ from model.group import Group
 import random
 import string
 
-# constant_data = [
-#     Group(name="name1", header="header1", footer="footer1"),
-#     Group(name="name2", header="header2", footer="footer2"),
-#     Group(name="name3", header="header3", footer="footer3"),
-# ]
-
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " " * 10
